5b8d71cbc077016c3e1bae00f5069e6fb0558b5d
chrisawgey <chrisvpopoca@gmail.com>
2024-06-19 20:40:19 -0400

    animation with title

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,13 @@
+// App.jsx
 import "./App.css";
 import Landing from "./Components/Landing";
-import Body from "./Components/body";
+import Body from "./Components/body"; 
 
 function App() {
     return (
         <div className="app">
-            <Landing></Landing>
-            <Body></Body>
+            <Landing />
+            <Body />
         </div>
     );
 }
