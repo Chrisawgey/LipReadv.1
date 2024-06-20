555480fb163ac9b6ab1ea109f5981429a160a2e6
chrisawgey <chrisvpopoca@gmail.com>
2024-06-19 20:07:21 -0400

    testing something

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Landing from "./Components/Landing";
+import Body from "./Components/body";
 
 function App() {
     return (
         <div className="app">
             <Landing></Landing>
+            <Body></Body>
         </div>
     );
 }
