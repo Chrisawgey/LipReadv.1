93cdeaba86a8125b8709997a61c194fd61b2badc
justinp24 <justinpardo2423@gmail.com>
2024-06-24 00:30:11 -0400

    finish section

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Landing from "./Components/Landing";
 import Body from "./Components/body";
 import BodyTwo from "./Components/lowerb";
+import Landingtwo from "./Components/Landingtwo";
 
 function App() {
     return (
         <div className="app">
             <Landing />
             <Body />
+            <Landingtwo></Landingtwo>
             <BodyTwo />
         </div>
     );
