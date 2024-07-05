2ec2b43a169d2bc5ef59086100c4550dc12da8a1
justinp24 <justinpardo2423@gmail.com>
2024-07-05 00:19:35 -0400

    finalize routes in main

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,11 +1,10 @@
-import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import Tennis from "./pages/Tennis/tennis.jsx";
 import Soccer from "./pages/Soccer/Soccer.jsx";
 import LipReading from "./pages/Lip-Reading/LipReading.jsx";
-
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
@@ -15,14 +14,14 @@ const router = createBrowserRouter([
         element: <App></App>,
         children: [
             { path: "/", element: <Homepage></Homepage> },
-            { path: "/Lip-Reading", element: }
-            { path: "/Soccer", element: <Cart></Cart> },
-            { path: "/Tennis", element: <Game></Game> },
+            { path: "/Lip-Reading", element: <LipReading></LipReading> },
+            { path: "/Soccer", element: <Soccer></Soccer> },
+            { path: "/Tennis", element: <Tennis></Tennis> },
         ],
     },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>,
 );
