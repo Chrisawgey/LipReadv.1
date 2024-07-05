25c7e501e51c9e7847287071932c91670b14bf4f
justinp24 <justinpardo2423@gmail.com>
2024-07-05 01:19:11 -0400

    fix routes perm

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,13 @@
 // App.jsx
 import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
+import { Outlet } from "react-router-dom";
 
 function App() {
     // will use outlet to handle routes
     return (
         <div className="app">
-            <Homepage></Homepage>
+            <Outlet></Outlet>
         </div>
     );
 }
